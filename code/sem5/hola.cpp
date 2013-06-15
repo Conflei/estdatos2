ee8f9a888bfff68799cd54b30b6756a2e15d9e70
LEOPiC <leopic@gmail.com>
2013-06-15 11:08:33 -0600

    Semana5.

---
+++ code/sem5/hola.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(void) {
+  int numero1, numero2;
+
+  cout << "\ndigite un numero: ";
+  cin >> numero1 >> numero2;
+  cout << "#1: " << numero1 << endl;
+  cout << "#2: " << numero2 << endl;
+
+  //cout << "\a\a\a" << endl;
+  //cout << "Sonido\a\tDe nuevo\a" << endl;
+  //cout << "formato" << setw(10) << 123 << endl;
+  //cout << "arriba" << endl << "abajo";
+  //cout << "arriba\nabajo\n";
+}
+
---
+++ tareas/sem-5/tarea.md
@@ -0,0 +1,2 @@
+Lista
+Pila/Cola
