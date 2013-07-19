183fc663e566befc9dd1e501fffa55e3e2d9257f
LEOPiC <leopic@gmail.com>
2013-07-19 00:05:06 -0600

    El algoritmo ya considera velocidad e imprime la ruta mas rapida.

--- tareas/proy1/ejem/ejemplo.cpp
+++ tareas/proy1/ejem/ejemplo.cpp
@@ -1,46 +1,26 @@
-/****************************************************
- ***Algoritmo: Dijkstra (One Source Shortest Path)
- ***Tipo: Grafos
- ***Autor: Jhosimar George Arias Figueroa
- ****************************************************/
-
-/*
-EJEMPLO DE INPUT
-5 9
-1 2 7
-1 4 2
-2 3 1
-2 4 2
-3 5 4
-4 2 3
-4 3 8
-4 5 5
-5 3 5
-1
-*/
-
-#include <stdio.h>
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <stdio.h>
+#include <vector>
+
 using namespace std;
 
 #define MAX 10005 //maximo numero de vértices
-#define Node pair< int , int > //definimos el nodo como un par( first , second ) donde first es el vertice adyacente y second el peso de la arista
+#define PuntoEnRuta pair< int , float > //definimos el nodo como un par( first , second ) donde first es el vertice adyacente y second el peso de la arista
 #define INF 1<<30 //definimos un valor grande que represente la distancia infinita inicial, basta conque sea superior al maximo valor del peso en alguna de las aristas
 
 //La cola de prioridad de C++ por default es un max-Heap (elemento de mayor valor en el tope)
 //por ello es necesario realizar nuestro comparador para que sea un min-Heap
 struct cmp {
-    bool operator() ( const Node &a , const Node &b ) {
+    bool operator() ( const PuntoEnRuta &a , const PuntoEnRuta &b ) {
         return a.second > b.second;
     }
 };
 
-vector< Node > ady[ MAX ]; //lista de adyacencia
-int distancia[ MAX ];      //distancia[ u ] distancia de vértice inicial a vértice con ID = u
+vector< PuntoEnRuta > ady[ MAX ]; //lista de adyacencia
+float distancia[ MAX ];      //distancia[ u ] distancia de vértice inicial a vértice con ID = u
 bool visitado[ MAX ];      //para vértices visitados
-priority_queue< Node , vector<Node> , cmp > ColaPrioridad; //priority queue propia del c++, usamos el comparador definido para que el de menor valor este en el tope
+priority_queue< PuntoEnRuta , vector<PuntoEnRuta> , cmp > ColaPrioridad; //priority queue propia del c++, usamos el comparador definido para que el de menor valor este en el tope
 int V;                     //numero de vertices
 int previo[ MAX ];         //para la impresion de caminos
 
@@ -54,16 +34,16 @@ void init() {
 }
 
 //Paso de relajacion
-void relajacion( int actual , int adyacente , int peso ) {
+void relajacion( int actual , int adyacente , float peso ) {
     //Si la distancia del origen al vertice actual + peso de su arista es menor a la distancia del origen al vertice adyacente
     if( distancia[ actual ] + peso < distancia[ adyacente ] ){
         distancia[ adyacente ] = distancia[ actual ] + peso;  //relajamos el vertice actualizando la distancia
         previo[ adyacente ] = actual;                         //a su vez actualizamos el vertice previo
-        ColaPrioridad.push( Node( adyacente , distancia[ adyacente ] ) ); //agregamos adyacente a la cola de prioridad
+        ColaPrioridad.push( PuntoEnRuta( adyacente , distancia[ adyacente ] ) ); //agregamos adyacente a la cola de prioridad
     }
 }
 
-//Impresion del camino mas corto desde el vertice inicial y final ingresados
+// Impresion del camino mas corto desde el vertice inicial y final ingresados
 void imprimirCamino(int destino) {
     if( previo[ destino ] != -1 ) {    //si aun poseo un vertice previo
         imprimirCamino( previo[ destino ] );  //recursivamente sigo explorando
@@ -72,11 +52,12 @@ void imprimirCamino(int destino) {
 }
 
 void dijkstra( int inicial ) {
-    init(); //inicializamos nuestros arreglos
-    ColaPrioridad.push( Node( inicial , 0 ) ); //Insertamos el vértice inicial en la Cola de Prioridad
-    distancia[ inicial ] = 0;      //Este paso es importante, inicializamos la distancia del inicial como 0
+    int actual, adyacente, destino;
+    float peso;
 
-    int actual , adyacente , peso;
+    init(); //inicializamos nuestros arreglos
+    ColaPrioridad.push( PuntoEnRuta( inicial , 0 ) ); //Insertamos el vértice inicial en la Cola de Prioridad
+    distancia[ inicial ] = 0.0;      //Este paso es importante, inicializamos la distancia del inicial como 0
 
     while( !ColaPrioridad.empty() ){                   //Mientras cola no este vacia
         actual = ColaPrioridad.top().first;            //Obtengo de la cola el nodo con menor peso, en un comienzo será el inicial
@@ -93,38 +74,29 @@ void dijkstra( int inicial ) {
         }
     }
 
-    printf( "Distancias mas cortas iniciando en vertice %d\n" , inicial );
-
-    for( int i = 1 ; i <= V ; ++i ){
-        printf("Vertice %d , distancia mas corta = %d\n" , i , distancia[ i ] );
-    }
-
-    puts("\n**************Impresion de camino mas corto**************");
+    puts("\n**************Impresion de camino mas rapido**************");
     printf("Ingrese vertice destino: ");
-    int destino;
     scanf("%d" , &destino );
+    printf("Vertice %d, tiempo mas corto = %f\n" , destino , distancia[destino] );
     imprimirCamino( destino );
     printf("\n");
 }
 
+int main() {
+    int totalConexiones , origen, destino , distancia, velocidad, inicial;
+    float tiempo;
 
-
-
-
-int main(){
-    int E , origen, destino , peso , inicial;
-    cout << "Ingrese la cantidad de nodos: ";
+    cout << "Ingrese la cantidad de puntos en ruta: ";
     scanf("%d" , &V );
 
-    cout << "Ingrese la cantidad de conexiones: ";
-    scanf("%d" , &E );
+    cout << "Ingrese el total de conexiones entre ellos: ";
+    scanf("%d" , &totalConexiones );
 
-    while( E-- ){
-        puts("Ingrese el origen | destino | peso");
-        scanf("%d %d %d" , &origen , &destino , &peso );
-        ady[ origen ].push_back( Node( destino , peso ) ); //consideremos grafo dirigido
-        //ady[ destino ].push_back( Node( origen , peso ) ); //grafo no dirigido
-
+    while( totalConexiones-- ) {
+        puts("Ingrese el origen | destino | distancia | velocidad: ");
+        scanf("%d %d %d %d" , &origen , &destino , &distancia, &velocidad);
+        tiempo = (float)(distancia)/(float)(velocidad);
+        ady[ origen ].push_back( PuntoEnRuta( destino , tiempo ) ); //consideremos grafo dirigido
     }
 
     printf("Ingrese el vertice inicial: ");
