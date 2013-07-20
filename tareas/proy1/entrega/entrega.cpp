d365a21b1d02bf3a7e49de416dd977e5c7d4e213
LEOPiC <leopic@gmail.com>
2013-07-19 23:43:40 -0600

    Removiendo 'puts', refactor de las variables.

--- tareas/proy1/entrega/entrega.cpp
+++ tareas/proy1/entrega/entrega.cpp
@@ -6,9 +6,9 @@
 using namespace std;
 
 #define MAX 10005 //maximo numero de vértices
-//#define PuntoEnRuta pair< int , float > //definimos el nodo como un par( , second ) donde first es el vertice adyacente y second el distancia de la arista
 #define INF 1<<30 //definimos un valor grande que represente la distancia infinita inicial, basta conque sea superior al maximo valor del distancia en alguna de las aristas
 
+// puntos en el mapa
 class Punto {
   public:
     Punto(int pAdyacente, int pDistancia, int pVelocidad, float pTiempo) : adyacente(pAdyacente), distancia(pDistancia), velocidad(pVelocidad), tiempo(pTiempo) {}
@@ -29,20 +29,20 @@ struct cmp {
     }
 };
 
-bool visitado[MAX];      //para vértices visitados
 //float distancia[MAX];      //distancia[ u ] distancia de vértice inicial a vértice con ID = u
+bool puntosVisitados[MAX];      //para vértices visitados
 int distancia[MAX];      //distancia[ u ] distancia de vértice inicial a vértice con ID = u
-int V;                     //numero de vertices
-int previo[MAX];         //para la impresion de caminos
+int puntosAnterioriores[MAX];         //para la impresion de caminos
+int totalPuntos;                     //numero de vertices
 priority_queue<Punto, vector<Punto>, cmp > ColaPrioridad; //priority queue propia del c++, usamos el comparador definido para que el de menor valor este en el tope
 vector<Punto> grafo[MAX]; //lista de adyacencia
 
 //función de inicialización
 void init() {
-    for( int i = 0 ; i <= V ; ++i ){
+    for( int i = 0 ; i <= totalPuntos; ++i ){
         distancia[i] = INF;  //inicializamos todas las distancias con valor infinito
-        visitado[i] = false; //inicializamos todos los vértices como no visitados
-        previo[i] = -1;      //inicializamos el previo del vertice i con -1
+        puntosVisitados[i] = false; //inicializamos todos los vértices como no visitados
+        puntosAnterioriores[i] = -1;      //inicializamos el previo del vertice i con -1
     }
 }
 
@@ -53,17 +53,17 @@ void relajacion( int actual , int adyacente , int peso) {
     if( distancia[ actual ] + peso < distancia[ adyacente ] ){
         //distancia[ adyacente ] = distancia[ actual ] + tiempo;  //relajamos el vertice actualizando la distancia
         distancia[ adyacente ] = distancia[ actual ] + peso;  //relajamos el vertice actualizando la distancia
-        previo[ adyacente ] = actual;                         //a su vez actualizamos el vertice previo
+        puntosAnterioriores[ adyacente ] = actual;                         //a su vez actualizamos el vertice previo
         ColaPrioridad.push(Punto(adyacente, distancia[adyacente])); //agregamos adyacente a la cola de prioridad
     }
 }
 
 // Impresion del camino mas corto desde el vertice inicial y final ingresados
 void imprimirCamino(int destino) {
-    if( previo[ destino ] != -1 ) {    //si aun poseo un vertice previo
-        imprimirCamino( previo[ destino ] );  //recursivamente sigo explorando
+    if( puntosAnterioriores[ destino ] != -1 ) {    //si aun poseo un vertice previo
+        imprimirCamino( puntosAnterioriores[ destino ] );  //recursivamente sigo explorando
     }
-    printf("%d ", destino);        //terminada la recursion imprimo los vertices recorridos
+    cout << destino << "->";
 }
 
 void dijkstra( int inicial ) {
@@ -78,49 +78,48 @@ void dijkstra( int inicial ) {
     while( !ColaPrioridad.empty() ){                   //Mientras cola no este vacia
         actual = ColaPrioridad.top().adyacente;            //Obtengo de la cola el nodo con menor peso, en un comienzo será el inicial
         ColaPrioridad.pop();                           //Sacamos el elemento de la cola
-        if (visitado[actual]) {
+        if (puntosVisitados[actual]) {
             continue; //Si el vértice actual ya fue visitado entonces sigo sacando elementos de la cola
         }
-        visitado[ actual ] = true;         //Marco como visitado el vértice actual
+        puntosVisitados[ actual ] = true;         //Marco como visitado el vértice actual
 
         for (int i = 0; i < grafo[actual].size(); ++i ){ //reviso sus adyacentes del vertice actual
             adyacente = grafo[ actual ][ i ].adyacente;   //id del vertice adyacente
             //tiempo = grafo[actual][i].tiempo;        //peso de la arista que une actual con adyacente ( actual , adyacente )
             peso = grafo[actual][i].distancia;        //peso de la arista que une actual con adyacente ( actual , adyacente )
-            if(!visitado[adyacente]) {        //si el vertice adyacente no fue visitado
+            if(!puntosVisitados[adyacente]) {        //si el vertice adyacente no fue visitado
                 relajacion(actual, adyacente, peso); //realizamos el paso de relajacion
             }
         }
     }
 
-    puts("\n**************Impresion de camino mas rapido**************");
-    printf("Ingrese vertice destino: ");
+    cout << "**************Impresion de camino mas corto**************" << endl;
+    cout << "Ingrese vertice destino: " << endl;
     scanf("%d" , &destino );
-    printf("Vertice %d, distancia mas corta = %d\n" , destino , distancia[destino] );
+    cout << "Para el punto " << destino << ", la distancia mas corta es = " << distancia[destino] << endl;
     imprimirCamino( destino );
-    printf("\n");
+    cout << endl;
 }
 
 int main() {
-    int totalConexiones , origen, destino , distancia, velocidad, inicial;
+    int totalRutas , origen, destino , distancia, velocidad, inicial;
     float tiempo;
 
     cout << "Ingrese la cantidad de puntos en ruta: ";
-    scanf("%d" , &V );
+    scanf("%d" , &totalPuntos);
 
-    cout << "Ingrese el total de conexiones entre ellos: ";
-    scanf("%d" , &totalConexiones );
+    cout << "Ingrese el total de rutas: ";
+    scanf("%d" , &totalRutas);
 
-    while( totalConexiones-- ) {
-        puts("Ingrese el origen | destino | distancia | velocidad: ");
+    while (totalRutas--) { // mientras el total de rutas sea mayor a cero
+        cout << "Ingrese la ruta en formato:  origen | destino | distancia | velocidad: " << endl;
         scanf("%d %d %d %d" , &origen , &destino , &distancia, &velocidad);
         //tiempo = (float)(distancia)/(float)(velocidad);
-        //cout << "tiempo en el while" << tiempo << endl;
         //grafo[origen].push_back( Punto( destino , tiempo ) ); //consideremos grafo dirigido
         grafo[origen].push_back( Punto( destino , distancia) ); //consideremos grafo dirigido
     }
 
-    printf("Ingrese el vertice inicial: ");
+    cout << "Ingrese el vertice inicial: " << endl;
     scanf("%d" , &inicial );
     dijkstra( inicial );
     return 0;
